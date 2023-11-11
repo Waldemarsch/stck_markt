785de1d25a2f4f342bd145c91e501e37a065d592
Waldemarsch <vovazms@gmail.com>
2023-11-11 21:56:55 +0300

    Added model

--- main_terminal/internal/models/stock.go
+++ main_terminal/internal/models/stock.go
@@ -4,6 +4,7 @@ type Currency struct {
 }
 
 type Stock struct {
+	SecID     string  `json:"secID"`
 	TradeNo   int64   `json:"tradeno"`
 	TradeTime string  `json:"tradetime"`
 	Price     float64 `json:"price"`
