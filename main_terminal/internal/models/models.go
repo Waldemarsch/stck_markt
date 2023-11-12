714ff1cb8b8099ecd83c3887f43c719df14908f4
Waldemarsch <vovazms@gmail.com>
2023-11-12 18:18:19 +0300

    Added company model

--- main_terminal/internal/models/models.go
+++ main_terminal/internal/models/models.go
@@ -9,3 +9,8 @@ type Stock struct {
 	TradeTime string  `json:"tradetime"`
 	Price     float64 `json:"price"`
 }
+
+type StockCompany struct {
+	Company string  `json:"company"`
+	Stocks  []Stock `json:"stocks"`
+}
