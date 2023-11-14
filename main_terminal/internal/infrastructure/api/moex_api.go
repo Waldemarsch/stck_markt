f2d777e6771e0f4258b9b2ade1c7928d1bdb3a74
Waldemarsch <vovazms@gmail.com>
2023-11-14 16:43:28 +0300

    Added ReadStocks MOEX API function

--- main_terminal/internal/infrastructure/api/moex_api.go
+++ main_terminal/internal/infrastructure/api/moex_api.go
@@ -1,1 +1,73 @@
 package api
+
+import (
+	"context"
+	"encoding/json"
+	"encoding/xml"
+	"github.com/carlmjohnson/requests"
+	"main_terminal/internal/models"
+)
+
+type MoexAPIRequest struct {
+	URL    string
+	Params map[string][]string
+}
+
+type MoexAPIResponse struct {
+	Document xml.Name `xml:"document"`
+	Data     struct {
+		Rows []struct {
+			Tradeno   string `xml:"TRADENO,attr" json:"tradeno"`
+			Tradetime string `xml:"TRADETIME,attr" json:"tradetime"`
+			Price     string `xml:"PRICE,attr" json:"price"`
+		} `xml:"rows" json:"stocks"`
+	} `xml:"data"`
+}
+
+type MoexAPI struct {
+	stockURL    string
+	currencyURL string
+}
+
+func NewMoexAPI(stockURL string, currencyURL string) *MoexAPI {
+	return &MoexAPI{
+		stockURL:    stockURL,
+		currencyURL: currencyURL,
+	}
+}
+
+func (a *MoexAPI) GetStocks(ctx context.Context, companies []models.StockCompany, params map[string][]string) ([]*models.StockCompany, error) {
+	var resultedCompanies []*models.StockCompany
+	for _, comp := range companies {
+		var res MoexAPIResponse
+		req := MoexAPIRequest{URL: a.stockURL, Params: params}
+		err := requests.URL(req.URL).Pathf("/%s/trades.json", comp.Company).Params(req.Params).ToDeserializer(xml.Unmarshal, &res).Fetch(ctx)
+
+		if err != nil {
+			return nil, err
+		}
+
+		resJSON, err := json.Marshal(res.Data.Rows)
+
+		if err != nil {
+			return nil, err
+		}
+
+		var stocksToResult []models.Stock
+		err = json.Unmarshal(resJSON, &stocksToResult)
+
+		if err != nil {
+			return nil, err
+		}
+
+		companyToResult := &models.StockCompany{
+			Company: comp.Company,
+			Stocks:  stocksToResult,
+		}
+
+		resultedCompanies = append(resultedCompanies, companyToResult)
+
+	}
+
+	return resultedCompanies, nil
+}
--- main_terminal/internal/models/models.go
+++ main_terminal/internal/models/models.go
@@ -4,7 +4,6 @@ type Currency struct {
 }
 
 type Stock struct {
-	SecID     string  `json:"secID"`
 	TradeNo   int64   `json:"tradeno"`
 	TradeTime string  `json:"tradetime"`
 	Price     float64 `json:"price"`
